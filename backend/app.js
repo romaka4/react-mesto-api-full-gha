574db6bfad7fd025d1716f45ff5cece9a704e258
romaka4 <rkafo@yandex.ru>
2024-02-01 19:40:54 +0300

    q

--- backend/app.js
+++ backend/app.js
@@ -6,19 +6,18 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const { requestLogger, errorLogger } = require('./middlewares/logger'); 
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-err');
 const { regExp } = require('./utils/regExp');
 
-
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const { PORT = 3005, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 mongoose.connect(DB_URL);
-app.use(cors());
 app.use(requestLogger);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
